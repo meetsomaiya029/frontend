6548e66b11c4d7d5e02bed10b294dcb4adfa7b5a
meetsomaiya029 <meetsomaiya029@gmail.com>
2026-09-10 22:59:47 +0530

    Update API_BASE_URL to include '/api' suffix

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://my-chat-app29.duckdns.org';
+export const API_BASE_URL = 'https://my-chat-app29.duckdns.org/api';
 
 // export const API_BASE_URL = 'http://localhost:3001';
